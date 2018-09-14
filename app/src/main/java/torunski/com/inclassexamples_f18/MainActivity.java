076ef12543767256d643fa93e0ee01771566d463
Eric Torunski <etorunski@gmail.com>
2018-09-14 14:00:46 -0400

    fixed null crash

--- app/src/main/java/torunski/com/inclassexamples_f18/MainActivity.java
+++ app/src/main/java/torunski/com/inclassexamples_f18/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -13,22 +14,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        final EditText input =  (EditText)findViewById(R.id.editText);
         Button b1 = (Button)findViewById(R.id.button1);
+
+        setContentView(R.layout.activity_main);
+
+
         Button b2 = (Button)findViewById(R.id.button2);
 
+        final TextView greeting = (TextView)findViewById(R.id.greeting);
+
+        try {
             b1.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          input.setText("You clicked button1");
-                                      }});
+                @Override
+                public void onClick(View v) {
+                    greeting.setText("You clicked button 1");
+                }
+            });
+        } catch (NullPointerException npe)
+        {
 
-        b2.setOnClickListener( e -> input.setText("You clicked button 2") );
+        }
+        b2.setOnClickListener( vw -> greeting.setText("You clicked button 2"));
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
 }
